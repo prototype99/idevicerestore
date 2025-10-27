d98b8ca9d876f4cfa7f8ce3aaf55d8a5239e3b22
Seirra <sophietheopossum@yandex.ru>
2025-10-27 16:32:04 +0000

    Fix build
    implements 914fbb35f97b37738a93f3f80fb9548bac7afcee & other related fixes that this older version needs

--- src/dfu.c
+++ src/dfu.c
@@ -95,7 +95,6 @@ int dfu_check_mode(struct idevicerestore_client_t* client, int* mode) {
 		return -1;
 	}
 
-	irecv_init();
 	if (irecv_open_with_ecid(&dfu, client->ecid) != IRECV_E_SUCCESS) {
 		return -1;
 	}
@@ -119,7 +118,6 @@ irecv_device_t dfu_get_irecv_device(struct idevicerestore_client_t* client) {
 	irecv_error_t dfu_error = IRECV_E_SUCCESS;
 	irecv_device_t device = NULL;
 
-	irecv_init();
 	if (irecv_open_with_ecid(&dfu, client->ecid) != IRECV_E_SUCCESS) {
 		return NULL;
 	}
--- src/recovery.c
+++ src/recovery.c
@@ -108,7 +108,6 @@ int recovery_check_mode(struct idevicerestore_client_t* client) {
 		return -1;
 	}
 
-	irecv_init();
 	recovery_error=irecv_open_with_ecid(&recovery, client->ecid);
 
 	if (recovery_error != IRECV_E_SUCCESS) {
